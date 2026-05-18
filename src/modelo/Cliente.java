81ea9306cbd84734795bf0077a389ea71f05cbda
Brabob <brandonbonillab2005@gmail.com>
2026-05-17 23:01:15 -0500

    test: casos de prueba unitarias ejecutados y documentados

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -13,7 +13,8 @@ public class Cliente implements Serializable {
     private String telefono;
     private String nombreContacto;
     private double porcentajeDescuento; // 0.0 - 70.0
-    private String casa;
+    private String territorio;
+    private int arroz; 
 
     public Cliente(char tipoIdentificacion, String numeroIdentificacion, boolean empresa,
                    String nombre, String email, String telefono,
